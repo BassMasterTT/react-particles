c51df6ab5423fe7c9ba2e1e6f9558fb20232f58a
BassMasterTT <tothtibi22@gmail.com>
2021-02-23 02:01:28 +0800

    particles and h1 under

--- src/App.js
+++ src/App.js
@@ -6,62 +6,24 @@ import Particles from 'react-particles-js';
 function App() {
   return (
     <div>
-      <div id="welcome" class="jumbotron">
-        <div class="container">
-          <h1 class="hero-title" class="load-hidden">
-            Hi, my name is <span class="text-color-main">Tibor Tóth</span>
-            <br />
-            I'm the Developer <span class="text-danger">You </span>need
-          </h1>
-          <p class="hero-cta" class="load-hidden">
-            <a class="cta-btn cta-btn--hero" href="#about">
-              Know more
-            </a>
-          </p>
-        </div>
-        <div>
-          <div id='about' class='about'>
+      <>
+        <Particles id="particles-js" />
 
+        <div id="hero" class="jumbotron">
+          <div class="container">
+            <h1 class="hero-title" class="load-hidden">
+              Hi, my name is <span class="text-color-main">Tibor Tóth</span>
+              <br />
+              I'm the Developer <span class="text-danger">You </span>need
+            </h1>
+            <p class="hero-cta" class="load-hidden">
+              <a class="cta-btn cta-btn--hero" href="#about">
+                Know more
+              </a>
+            </p>
           </div>
         </div>
-      </div>
-      <Particles
-        id="particles-js"
-        // params={{
-        //   particles: {
-        //     number: {
-        //       value: 400,
-        //       density: {
-        //         enable: true,
-        //         value_area: 1000,
-        //       },
-        //     },
-        //     color: {
-        //       value: '#fff',
-        //     },
-        //     opacity: {
-        //       value: 0.5,
-        //       anim: {
-        //         enable: true,
-        //       },
-        //     },
-        //     size: {
-        //       value: 7,
-        //       random: true,
-        //       anim: {
-        //         enable: true,
-        //         speed: 3,
-        //       },
-        //     },
-        //     line_linked: {
-        //       enable: false,
-        //     },
-        //     move: {
-        //       speed: 0.2,
-        //     },
-        //   },
-        // }}
-      />
+      </>
     </div>
   );
 }
