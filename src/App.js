e817162ef27b03afbb9ef06196425e6c7052ec53
BassMasterTT <tothtibi22@gmail.com>
2021-02-23 22:47:26 +0800

    particles working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import Particles from 'react-particles-js';
@@ -6,24 +5,92 @@ import Particles from 'react-particles-js';
 function App() {
   return (
     <div>
-      <>
-        <Particles id="particles-js" />
-
-        <div id="hero" class="jumbotron">
-          <div class="container">
-            <h1 class="hero-title" class="load-hidden">
-              Hi, my name is <span class="text-color-main">Tibor Tóth</span>
-              <br />
-              I'm the Developer <span class="text-danger">You </span>need
-            </h1>
-            <p class="hero-cta" class="load-hidden">
-              <a class="cta-btn cta-btn--hero" href="#about">
-                Know more
-              </a>
-            </p>
-          </div>
-        </div>
-      </>
+      <div className="scale-in-center" id="welcome">
+        <h1>Welcome</h1>
+      </div>
+      <Particles
+        id="particles-js"
+        params={{
+          particles: {
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: '#424deb',
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: '#fcfcfc'
+              },
+              polygon : {
+                nb_sides: 5
+              },
+              image: {
+                src: 'img/github.svg',
+                width: 100,
+                height: 100
+              }
+            },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+              },
+            },
+            size: {
+              value: 4,
+              random: true,
+              anim: {
+                enable: true,
+                speed: 5,
+                size_min: 4.86914181375533,
+                sync: false
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: '#424deb',
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: 'none',
+              random: true,
+              straight: false,
+              out_model: 'out',
+              bounce: false,
+              attract: {
+                enable: false,
+                rotatex: 600,
+                rotateY: 1200
+              }
+            },
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'repulse'
+              },
+              
+            }
+          }
+        }}
+      />
     </div>
   );
 }
