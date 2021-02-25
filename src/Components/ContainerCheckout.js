32afd14e7bf3178ba051ea5cd1270ce32a29ac32
BassMasterTT <tothtibi22@gmail.com>
2021-02-26 01:59:59 +0800

    another card under the main

---
+++ src/Components/ContainerCheckout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ContainerCheckout.css';
+
+function ContainerCheckout() {
+  return (
+    <div>
+      <div class="card">
+        <div className="container">
+          <p>Check out the cool tilf effect</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContainerCheckout;
