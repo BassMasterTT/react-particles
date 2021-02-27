c51da998c807a4fe59685a9d3f19e930b3704b75
BassMasterTT <tothtibi22@gmail.com>
2021-02-27 12:54:22 +0800

    Projects component

---
+++ src/Components/Page2.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Page2.css';
+
+function Page2() {
+  return <div className='projectsContainer'>
+      <h1 className='projectsTitle'>PROJECTS</h1>
+  </div>;
+}
+
+export default Page2;
