ce73a9fdba025478a68e2dde040fb9bb1747da2f
BassMasterTT <tothtibi22@gmail.com>
2021-03-02 02:51:34 +0800

    footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footerDiv">
+            <h1 className='h1footer'>Let's get in touch</h1>
+        </div>
+    )
+}
+
+export default Footer
